beb33fd516e76810e0bc4e81839b71396e94cd4e
Tiago Silva <tiago.nicole@hotmail.com>
2022-03-28 17:42:52 +0100

    a)

--- Trabalho 1/Python/Exercicio2Trabalho1.py
+++ Trabalho 1/Python/Exercicio2Trabalho1.py
@@ -1,21 +1,29 @@
 import random
 import matplotlib.pyplot as plt
+import string
 
-def stringsource(dictionary, probability):
+def stringsource(dictionary, probability, repeat, hist):
     file = open("stringoutput", 'w')
-    result = random.choices(dictionary, weights=probability, k=1000)
+    result = random.choices(dictionary, weights=probability, k=repeat)
     countresults = [0]*len(dictionary)
-
     idx = 0
     for i in dictionary:
         countresults[idx] = result.count(i)
         idx += 1
-
     for i in result:
         file.write(i + ";")
     file.close()
-    plt.bar(dictionary, countresults, width=0.5)
-    plt.show()
+    if(hist):
+        plt.bar(dictionary, countresults, width=0.5)
+        plt.show()
+    return result
+
+def passGen(min, max):
+    size = random.randint(min, max)
+    letters = list(string.ascii_letters + string.digits + string.punctuation)
+    probability = [1/len(letters)]*len(letters)
+    print(stringsource(letters, probability, size, False))
 
 
-stringsource(["aaa", "bbb", "ccc"], [0.25, 0.25, 0.50])
+stringsource(["aaa", "bbb", "ccc", "ddd"], [0.25, 0.25, 0.25, 0.25], 1000, hist=False)
+passGen(10, 15)
