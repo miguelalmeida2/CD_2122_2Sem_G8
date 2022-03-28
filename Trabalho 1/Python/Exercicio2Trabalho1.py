24e90cfcda72be69e42f135ae1265c52f1780b2c
Tiago Silva <tiago.nicole@hotmail.com>
2022-03-28 17:15:42 +0100

    a)

--- Trabalho 1/Python/Exercicio2Trabalho1.py
+++ Trabalho 1/Python/Exercicio2Trabalho1.py
@@ -1,5 +1,21 @@
-def stringsource(dictionary, function):
-    file = open(dictionary)
+import random
+import matplotlib.pyplot as plt
+
+def stringsource(dictionary, probability):
+    file = open("stringoutput", 'w')
+    result = random.choices(dictionary, weights=probability, k=1000)
+    countresults = [0]*len(dictionary)
+
+    idx = 0
+    for i in dictionary:
+        countresults[idx] = result.count(i)
+        idx += 1
+
+    for i in result:
+        file.write(i + ";")
+    file.close()
+    plt.bar(dictionary, countresults, width=0.5)
+    plt.show()
 
 
-stringsource("StringDictionary")
+stringsource(["aaa", "bbb", "ccc"], [0.25, 0.25, 0.50])
