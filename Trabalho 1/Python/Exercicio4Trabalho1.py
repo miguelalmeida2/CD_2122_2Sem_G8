4ac6eef8e89e2573ed681bd949b04c761d9b1795
Tiago Silva <tiago.nicole@hotmail.com>
2022-04-18 19:16:49 +0100

    More LZ77 Compression

--- Trabalho 1/Python/Exercicio4Trabalho1.py
+++ Trabalho 1/Python/Exercicio4Trabalho1.py
@@ -1,43 +1,41 @@
 local_path = "../CD_TestFiles/"
+test_path = "Test Files/"
 
 
 def LZ77_Tokenizer(filename: str, window_size: int, buffer_size: int):
-    file = open(local_path + filename)
-    dictionary = ""
-    lab = file.read(buffer_size)
-    while lab != "":
-        index, lenght, innovation_symbol = LZ77_search(dictionary, lab)
-        print("("+str(max(0, index))+", "+str(max(0, lenght))+", "+innovation_symbol+")")
-        dictionary += lab[:lenght+1]
-        lab = lab.removeprefix(lab[:lenght+1])
-        lab += file.read(lenght+1)
-        dictionary = trimDictionary(dictionary, window_size)
-    print(dictionary)
+    inputText = open(local_path + filename).read()
+    outputFile = open(test_path + "TOKENS_" + filename, 'w')
+    dictIterator = 0
+    labIterator = 0
+    while labIterator < len(inputText):
+        dictionary = inputText[dictIterator:labIterator]
+        lab = inputText[labIterator:labIterator+buffer_size]
+        offset, length, char = LZ77_search(dictionary, lab)
+        print("(" + str(offset) + ", " + str(length) + ", " + char + ")")
+        outputFile.write("(" + str(offset) + ", " + str(length) + ", " + char + ")" + "\n")
+        labIterator = labIterator + length + 1
+        dictIterator = labIterator - window_size
+        if dictIterator < 0:
+            dictIterator = 0
 
 
-def trimDictionary(window, max_size):
-    while len(window) > max_size:
-        window = window.removeprefix(window[0])
-    return window
+def LZ77_search(dictionary, lab):
+    dictLength = len(dictionary)
+    labLength = len(lab)
 
+    if dictLength == 0:
+        return 0, 0, lab[0]
 
-def LZ77_search(search, look_ahead):
-    ls = len(search)
-    llh = len(look_ahead)
-
-    if ls == 0:
-        return 0, 0, look_ahead[0]
-
-    if llh == 0:
+    if labLength == 0:
         return -1, -1, ""
 
     best_length = 0
     best_offset = 0
-    buf = search + look_ahead
+    buf = dictionary + lab
 
-    search_pointer = ls
-    print("search: ", search, " lookahead: ", look_ahead)
-    for i in range(0, ls):
+    search_pointer = dictLength
+    print("search: ", dictionary, " lookahead: ", lab)
+    for i in range(0, dictLength):
         length = 0
         while buf[i + length] == buf[search_pointer + length]:
             length = length + 1
@@ -53,8 +51,7 @@ def LZ77_search(search, look_ahead):
     return best_offset, best_length, buf[search_pointer + best_length]
 
 
-outputFile = open("Test Files/a_decompressed.txt", 'w')
-LZ77_Tokenizer("b.txt", 4, 2)
+LZ77_Tokenizer("person.java", 16, 8)
 """ 
 dict = 
 lab = aa
