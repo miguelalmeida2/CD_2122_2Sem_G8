319f3121f14ebef13db4e6aeea2b745dbbab250f
Tiago Silva <tiago.nicole@hotmail.com>
2022-04-28 14:47:03 +0100

    More LZ77 Compression

--- Trabalho 1/Python/Exercicio4Trabalho1.py
+++ Trabalho 1/Python/Exercicio4Trabalho1.py
@@ -1,3 +1,6 @@
+import math
+from matplotlib import pyplot as plt
+
 local_path = "../CD_TestFiles/"
 test_path = "Test Files/"
 
@@ -5,18 +8,42 @@ test_path = "Test Files/"
 def LZ77_Tokenizer(filename: str, window_size: int, buffer_size: int):
     inputText = open(local_path + filename).read()
     outputFile = open(test_path + "TOKENS_" + filename, 'w')
+    histPosition = [0]*window_size
+    histLength = [0]*buffer_size
     dictIterator = 0
     labIterator = 0
+    lengthEntropy = 0
+    positionEntropy = 0
+    totalTokens: float = 0
     while labIterator < len(inputText):
         dictionary = inputText[dictIterator:labIterator]
         lab = inputText[labIterator:labIterator+buffer_size]
         offset, length, char = LZ77_search(dictionary, lab)
+        histPosition[offset] += 1
+        histLength[length] += 1
         print("(" + str(offset) + ", " + str(length) + ", " + char + ")")
         outputFile.write("(" + str(offset) + ", " + str(length) + ", " + char + ")" + "\n")
         labIterator = labIterator + length + 1
         dictIterator = labIterator - window_size
+        totalTokens += 1
         if dictIterator < 0:
             dictIterator = 0
+    for value in histLength:
+        lengthProbablity = value / totalTokens
+        if lengthProbablity != 0:
+            lengthEntropy += lengthProbablity * math.log(1 / lengthProbablity, 2)
+    for value in histPosition:
+        positionProbablity = value / totalTokens
+        if positionProbablity != 0:
+            positionEntropy += positionProbablity * math.log(1 / positionProbablity, 2)
+
+    plt.bar(range(0, window_size), histPosition, 0.5)
+    plt.show()
+    print("Position Entropy:", positionEntropy)
+
+    plt.bar(range(0, buffer_size), histLength, 0.5)
+    plt.show()
+    print("Length Entropy:", lengthEntropy)
 
 
 def LZ77_search(dictionary, lab):
@@ -34,7 +61,7 @@ def LZ77_search(dictionary, lab):
     buf = dictionary + lab
 
     search_pointer = dictLength
-    print("search: ", dictionary, " lookahead: ", lab)
+    # print("search: ", dictionary, " lookahead: ", lab)
     for i in range(0, dictLength):
         length = 0
         while buf[i + length] == buf[search_pointer + length]:
@@ -51,21 +78,4 @@ def LZ77_search(dictionary, lab):
     return best_offset, best_length, buf[search_pointer + best_length]
 
 
-LZ77_Tokenizer("person.java", 16, 8)
-""" 
-dict = 
-lab = aa
-(0, 0, a)
-dict = a
-lab = ab
-(0, 1, b)
-dict = aa
-lab = bb
-(0, 0, b)
-dict = aab
-lab = ba
-(0, 1, a)
-dict = aabb
-lab = aa
-(0, 1, a)
-"""
+LZ77_Tokenizer("cp.htm", 16, 4)
