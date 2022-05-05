5a5f9cbc960eec0a3e4fd4879ef0621f22af98f7
Tiago Silva <tiago.nicole@hotmail.com>
2022-05-05 14:18:36 +0100

    Compression Rate Fixed

--- Trabalho 1/Python/FileCompressionRate.py
+++ Trabalho 1/Python/FileCompressionRate.py
@@ -15,6 +15,6 @@ def compressionRate(file1: str, file2: str):
 testFiles = ["a.txt", "alice29.txt", "cp.htm", "Person.java", "progc.c"]
 resultFiles = ["a.txt_encoded", "alice29.txt_encoded", "cp.htm_encoded", "Person.java_encoded", "progc.c_encoded"]
 for i in range(0, 4):
-    first = open(local_path + testFiles[i])
-    secound = open(test_path + resultFiles[i])
+    first = local_path + testFiles[i]
+    secound = test_path + resultFiles[i]
     print(compressionRate(first, secound))
