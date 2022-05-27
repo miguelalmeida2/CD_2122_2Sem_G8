d55f67916b5329eeabce0f2e12be70db4afe443e
Miguel Almeida 47249 <75702011+miguelalmeida2@users.noreply.github.com>
2022-05-27 10:28:02 +0100

    Ceaser Done

---
+++ Trabalho 2/Python/CeaserCipher.py
@@ -0,0 +1,44 @@
+import codecs
+import string
+import math
+
+#USEFUL DATA
+ASCSII_SIZE = 255
+local_path = "../CD_TestFiles/"
+test_path = "Test_Files/"
+CEASER_DISTANCE = 2
+
+#Implementations
+def ceaser_cipher(file):
+    fileReadDir = local_path + file
+    fileRead = codecs.open(fileReadDir, 'r', 'cp1252')
+    text = fileRead.read()
+    fileWriteDir = test_path + file + "_encoded"
+    fileWrite = codecs.open(fileWriteDir, 'w', 'cp1252')
+    for i in range(len(text)):
+        char = text[i]
+        charIndex = ord(char)
+        ceaserChar = chr((charIndex + CEASER_DISTANCE) % ASCSII_SIZE)
+        fileWrite.write(ceaserChar)
+    fileRead.close()
+    fileWrite.close()
+
+def ceaser_decipher(file):
+    fileReadDir = test_path + file + "_encoded"
+    fileRead = codecs.open(fileReadDir, 'r', 'cp1252')
+    text = fileRead.read()
+    fileWriteDir = test_path + file + "_decoded"
+    fileWrite = codecs.open(fileWriteDir, 'w', 'cp1252')
+    for i in range(len(text)):
+        char = text[i]
+        charIndex = ord(char)
+        ceaserChar = chr((charIndex - CEASER_DISTANCE) % ASCSII_SIZE)
+        fileWrite.write(ceaserChar)
+    fileRead.close()
+    fileWrite.close()
+
+#Execution
+ceaser_cipher("a.txt")
+ceaser_decipher("a.txt")
+ceaser_cipher("alice29.txt")
+ceaser_decipher("alice29.txt")
