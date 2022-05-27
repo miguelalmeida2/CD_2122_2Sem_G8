0b51288c5e11c8993dccc233682d7604fbcdfb79
Miguel Almeida 47249 <75702011+miguelalmeida2@users.noreply.github.com>
2022-05-27 18:42:47 +0100

    Create BMPImageCipher.py

---
+++ Trabalho 2/Python/BMPImageCipher.py
@@ -0,0 +1,57 @@
+import codecs
+import string
+import math
+import struct
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
+
+# USEFUL DATA
+local_path = "../CD_TestFiles/"
+test_path = "../Test_Output/"
+
+def bmpDecipher(file):
+    path = test_path + file
+    # taking encryption key as input
+    key = 37
+    # open file for reading purpose
+    fin = open(path, 'rb')
+    # storing image data in variable "image"
+    image = fin.read()
+    fin.close()
+    # converting image into byte array to
+    # perform encryption easily on numeric data
+    image = bytearray(image)
+    # performing XOR operation on each value of bytearray
+    for index, values in enumerate(image):
+        image[index] = values ^ key
+    # opening file for writing purpose
+    fin = open(path, 'wb')
+    # writing encrypted data in image
+    fin.write(image)
+    fin.close()
+
+
+def bmpCipher(file):
+    path = local_path + file
+# taking encryption key as input
+    key = 37
+    # open file for reading purpose
+    fin = open(path, 'rb')
+    # storing image data in variable "image"
+    image = fin.read()
+    fin.close()
+    # converting image into byte array to
+    # perform encryption easily on numeric data
+    image = bytearray(image)
+    # performing XOR operation on each value of bytearray
+    for index, values in enumerate(image):
+        image[index] = values ^ key
+    # opening file for writing purpose
+    fin = open(path, 'wb')
+    # writing encrypted data in image
+    fin.write(image)
+    fin.close()
+
+bmpCipher("lena.bmp")
+#bmpDecipher("lena.bmp")
