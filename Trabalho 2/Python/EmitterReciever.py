9d2ddde29d3a052f59cbb3e423ffe34ac59b85b0
Tiago Silva <tiago.nicole@hotmail.com>
2022-06-24 18:00:57 +0100

    3 a)

--- Trabalho 2/Python/EmitterReciever.py
+++ Trabalho 2/Python/EmitterReciever.py
@@ -2,33 +2,83 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def nrzu_coder(data: list):
-    for bit in data:
-        if bit:
-            print(1)
+def nrzu_coder(data, amp=1):
+    t = np.arange(0, len(data), .01)
+    j = 1
+    p = []
+    for i in range(len(data) * 100):
+        if t[i] < j:
+            p.append(data[j - 1] * amp)
         else:
-            print(0)
+            p.append(data[j] * amp)
+            j += 1
+
+    plt.plot(t, p, color='k')
+    plt.xlabel("Time in ms")
+    plt.ylabel("Amplitude in Volts")
+    plt.title("NRZ Unipolar")
+    plt.show()
+    return p
 
 
-def psk_modulator(data: list):
-    n = 10
+def nrzu_decoder(data):
+    bits = [0]*(int(len(data)/100))
+    idx = 0
+    for i in range(0, len(data), 100):
+        if data[i] > 0:
+            bits[idx] = 1
+        else:
+            bits[idx] = 0
+        idx += 1
+    return bits
 
 
-b = [np.random.randint(0, 2) for i in range(2500)]
-t = np.arange(0, 25, .01)
-j = 1
-p = []
-for i in range(2500):
-    if t[i] < j:
-        p.append(b[j-1])
-    else:
-        p.append(b[j])
-        j += 1
+def psk_modulator(data, amp=1, freq=1000):
+    bipolar = (2 * data - 1) * amp
+    bit_duration = 1
+    amplitude_scaling_factor = bit_duration / 2
+    n_samples = 2000
+    time = np.linspace(0, len(data), n_samples)
 
-plt.plot(t, p, color='k')
-plt.xlabel("Time")
-plt.ylabel("Value")
-plt.title("NRZ Unipolar")
-plt.show()
+    samples_per_bit = n_samples / len(data)
+    bb = np.repeat(bipolar, samples_per_bit)
+    waveform = np.sqrt(2 * amplitude_scaling_factor / bit_duration) * np.cos(2 * np.pi * freq * time)
+    bpsk_w = bb * waveform
+
+    plt.plot(time, bpsk_w, color='k')
+    plt.show()
+    return bb, amp
 
 
+def psk_demodulator(data, amp):
+    bits = [0]*(int(len(data)/250))
+    idx = 0
+    for i in range(0, len(data), 250):
+
+        if data[i] > 0:
+            bits[idx] = 1
+        else:
+            bits[idx] = 0
+        idx += 1
+
+    if amp < 0:
+        for i in range(0, len(bits)):
+            if bits[i] > 0:
+                bits[i] = 0
+            else:
+                bits[i] = 1
+    return bits
+
+
+bits = np.array([1, 0, 1, 1, 0, 0, 0, 1])
+noise_constant = np.array([1, 1, 1, 1, 1, 1, 1, 1])
+noise_random = np.array([np.random.randint(0, 2) for i in range(8)])
+
+nrzu_signal = nrzu_coder(bits, amp=5)
+print(nrzu_decoder(nrzu_signal))
+
+psk_signal, amp = psk_modulator(bits, amp=2, freq=2000)
+print(psk_demodulator(psk_signal, amp))
+
+psk_signal, amp = psk_modulator(bits, amp=-2, freq=2000)
+print(psk_demodulator(psk_signal, amp))
