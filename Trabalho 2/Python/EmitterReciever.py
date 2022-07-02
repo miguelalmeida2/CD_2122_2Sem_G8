c2ab23fe5d6b67760ea6346b23407d3eedfdffbf
Tiago Silva <tiago.nicole@hotmail.com>
2022-07-02 11:33:45 +0100

    3 b)

--- Trabalho 2/Python/EmitterReciever.py
+++ Trabalho 2/Python/EmitterReciever.py
@@ -52,7 +52,9 @@ def psk_modulator(data, amp=1.0, freq=1000):
 
     samples_per_bit = n_samples / len(data)
     bb = np.repeat(bipolar, samples_per_bit)
+    # Generate cos signal
     waveform = np.sqrt(2 * amplitude_scaling_factor / bit_duration) * np.cos(2 * np.pi * freq * time)
+    # Generate signal based on input and cos signal
     bpsk_w = bb * waveform
 
     plt.axis([0, len(data), -3, 3])
@@ -69,16 +71,17 @@ def psk_modulator(data, amp=1.0, freq=1000):
 
 def psk_demodulator(data, amp):
     amp = amp / 2
+    # Each bit gets represented by 2 points in plot
     data_bits = [0]*(int(len(data)/2))
     idx = 0
     for i in range(0, len(data), 2):
-
         if data[i] > amp:
             data_bits[idx] = 1
         else:
             data_bits[idx] = 0
         idx += 1
 
+    # If amplitude is negative reverse 1's and 0's.
     if amp < 0:
         for i in range(0, len(data_bits)):
             if data_bits[i] > 0:
@@ -97,7 +100,11 @@ def calc_ber(original, received):
     return error / float(size)
 
 
+# For a)
 # bits = np.array([1, 0, 1, 1, 0, 0, 0, 1])
+
+# For b)
+# Read all bytes in file and convert it into an array of bits.
 with open(local_path + "a.txt", 'rb') as file:
     text = file.read()
 
@@ -108,31 +115,51 @@ b = [int(i) for i in b]
 bits = np.array(list(b))
 
 # α = 1
+# Generate random noise signal between -1 and 1.
 noise_random = np.array([rnd.uniform(-1, 1) for i in range(len(bits))])
 
+# Generate signal based on file + noise.
 nrzu_signal, amplitude = nrzu_coder(bits + noise_random, amp=5)
+# Try to decode signal.
 nrzu_decoded = nrzu_decoder(nrzu_signal, amplitude)
+# Calculate BER for the decoded signal.
 print("BER: " + str(calc_ber(bits, nrzu_decoded)))
 
+# Generate signal based on file + noise.
 psk_signal, amplitude = psk_modulator(bits + noise_random, amp=2, freq=2000)
+# Try to decode signal.
 psk_decoded = psk_demodulator(psk_signal, amplitude)
+# Calculate BER for the decoded signal.
 print("BER: " + str(calc_ber(bits, psk_decoded)))
 
+# Generate signal based on file + noise.
 psk_signal, amplitude = psk_modulator(bits + noise_random, amp=-2, freq=2000)
+# Try to decode signal.
 psk_decoded = psk_demodulator(psk_signal, amplitude)
+# Calculate BER for the decoded signal.
 print("BER: " + str(calc_ber(bits, psk_decoded)))
 
 # α = 0.5
+# Generate constant noise signal
 noise_constant = np.array([1]*len(bits))*0.5
 
+# Generate signal based on file + noise.
 nrzu_signal, amplitude = nrzu_coder(bits + noise_constant, amp=5*0.5)
+# Try to decode signal.
 nrzu_decoded = nrzu_decoder(nrzu_signal, amplitude)
+# Calculate BER for the decoded signal.
 print("BER: " + str(calc_ber(bits, nrzu_decoded)))
 
+# Generate signal based on file + noise.
 psk_signal, amplitude = psk_modulator(bits + noise_constant, amp=2*0.5, freq=2000)
+# Try to decode signal.
 psk_decoded = psk_demodulator(psk_signal, amplitude)
+# Calculate BER for the decoded signal.
 print("BER: " + str(calc_ber(bits, psk_decoded)))
 
+# Generate signal based on file + noise.
 psk_signal, amplitude = psk_modulator(bits + noise_constant, amp=-2*0.5, freq=2000)
+# Try to decode signal.
 psk_decoded = psk_demodulator(psk_signal, amplitude)
+# Calculate BER for the decoded signal.
 print("BER: " + str(calc_ber(bits, psk_decoded)))
